ea11beb127a6e42050f041bd00b4d73dfeff07aa
Prisca <prisca.castanier@gmail.com>
2024-08-26 23:47:20 +0200

    feat(models): update models to add BasicMixins , update import and add datas

--- api/app/core/models/Url.py
+++ api/app/core/models/Url.py
@@ -1,22 +1,19 @@
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
 
-from api.app.core.database import Base
+from .utils.Base import Base
+from .utils.BasicMixins import BasicMixins
 
 
-class Url(Base):
+class Url(Base, BasicMixins):
     __tablename__ = "urls"
 
     id = Column(String, primary_key=True, index=True)
     url = Column(String, index=True)
     short_url = Column(String, index=True)
-    created_at = Column(String)
-    updated_at = Column(String)
     user_id = Column(String, ForeignKey("users.id"))
     clicks = Column(Integer)
     is_active = Column(Boolean, default=True)
-    is_deleted = Column(Boolean, default=False)
-    deleted_at = Column(String)
     deleted_by = Column(String)
     updated_by = Column(String)
     created_by = Column(String)
--- api/app/core/models/User.py
+++ api/app/core/models/User.py
@@ -1,14 +1,16 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.orm import relationship
 
-from api.app.core.database import Base
+from .utils.Base import Base
+from .utils.BasicMixins import BasicMixins
 
 
-class User(Base):
+class User(Base, BasicMixins):
     __tablename__ = "users"
 
     id = Column(String, primary_key=True, index=True)
     username = Column(String, unique=True, index=True)
     email = Column(String, unique=True, index=True)
     password = Column(String)
-    is_active = Column(Boolean, default=True)
+    last_password = Column(String, nullable=True)
+    last_login_at = Column(DateTime, nullable=True)
