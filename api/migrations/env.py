fbb44b4a7a52b24ff34e3cb3d0b7aa71ee80ab67
Prisca <prisca.castanier@gmail.com>
2024-08-26 23:42:30 +0200

    feat: import Base into env.py to use auto generate migration

--- api/migrations/env.py
+++ api/migrations/env.py
@@ -1,10 +1,14 @@
 import os
+
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+
+from app.core.models.utils.Base import Base
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -21,7 +25,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
