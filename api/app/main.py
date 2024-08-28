f248509e9ce97acabdbe028b4195c94ec7a78079
Prisca <prisca.castanier@gmail.com>
2024-08-29 00:52:27 +0200

    feat(api): add middleware to handle http requests

--- api/app/main.py
+++ api/app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 import api.app.core.models
+from api.app.core.config import Config
 from api.app.core.routes.UrlRouter import UrlRouter
 from api.app.core.routes.UserRouter import UserRouter
 
@@ -11,6 +13,15 @@ class AppFactory:
 
     def run(self):
         self.register_routes()
+
+        self.app.add_middleware(
+            CORSMiddleware,
+            allow_origins=Config.CORS_ORIGINS,
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=Config.HEADERS,
+        )
+
         return self.app
 
     def register_routes(self):
