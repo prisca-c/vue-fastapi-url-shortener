a85c0a57fd8f83fcb45d4f93715abdb358f729ed
Prisca <prisca.castanier@gmail.com>
2024-08-29 22:26:00 +0200

    refactor(url): update schemas

--- api/app/core/schemas/UrlSchemas.py
+++ api/app/core/schemas/UrlSchemas.py
@@ -4,18 +4,19 @@ from pydantic import UUID4
 
 class UrlBase(BaseModel):
     url: str
-    short_url: str
-    created_by: str
+    name: str
 
 
 class UrlCreate(UrlBase):
-    name: str
+    pass
 
 
 class Url(UrlBase):
     id: UUID4
     clicks: int
     is_deleted: bool
+    short_url: str
+    created_by: str | None = None
 
     class Config:
         orm_mod = True
